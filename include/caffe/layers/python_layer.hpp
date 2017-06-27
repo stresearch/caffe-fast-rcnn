5a4cf404711d1a845d08f0627651833957671f1f
Nate Crosswhite <nate.crosswhite@stresearch.com>
2017-06-27 11:46:44 -0400

    Fix for missing python attribute
    https://github.com/rbgirshick/py-faster-rcnn/issues/249

--- include/caffe/layers/python_layer.hpp
+++ include/caffe/layers/python_layer.hpp
@@ -26,7 +26,8 @@ class PythonLayer : public Layer<Dtype> {
     }
     self_.attr("param_str_") = bp::str(
         this->layer_param_.python_param().param_str());
-    self_.attr("phase") = static_cast<int>(this->phase_);
+    // See: https://github.com/rbgirshick/py-faster-rcnn/issues/249
+    // self_.attr("phase") = static_cast<int>(this->phase_);
     self_.attr("setup")(bottom, top);
   }
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
